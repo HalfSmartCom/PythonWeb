7363a2918a7ea1eef266b06149b43cb315370a19
yaole817 <yaole817@163.com>
2018-09-19 21:16:40 +0800

    添加了粘包的解决办法
    Signed-off-by: yaole817 <yaole817@163.com>

--- ChatRoom/client.py
+++ ChatRoom/client.py
@@ -1,37 +1,52 @@
 import socket
 import time
+import struct
+import json
 
+ip_port = ("127.0.0.1", 9999)
 
 class ChatClient:
-    def __init__(self, username, port):
+    def __init__(self, username):
         self.username = username
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect(("127.0.0.1", port))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建TCP 对象
+        self.socket.connect(ip_port)                                    # 连接server
+        self.package = {                   # 一个package信息
+            "msg" : None,
+            "name" : self.username,
+        }
 
     def send_msg(self, msg):
-        self.socket.send("{username}::{msg}".format(username=self.username,msg=msg).encode("utf-8"))
+        self.package["msg"] = msg            # update 字典中的信息
+        j_msg = json.dumps(self.package)     # 将字典转换为json 格式
+        byte_length = struct.pack("i", len(j_msg)) # 计算字典的长度，并以编码为固定长度
+        self.socket.send(byte_length)              # 发送json 的长度
+        self.socket.send(j_msg.encode("utf-8"))   # 发送数据
 
     def recv_msg(self):
-        data=self.socket.recv(1024)
+        info_length = self.socket.recv(4)              # 接收 server端的信息
+        data_length = struct.unpack("i", info_length)[0] # 转换为数据长度
+        print(data_length)
+        data = self.socket.recv(data_length).decode("utf-8")            # 接收一个package
+
         if data:
             print("\n【机器人小图】"+" "+time.strftime('%Y-%m-%d:%H:%M:%S',time.localtime(time.time())))
-            print(data.decode("utf-8"))
+            print(data)
             return True
         return False
 
     def main(self):
-        data = self.socket.recv(1024)
-        print(data.decode("utf-8"))
-        msg = input("请输入消息：")
-        self.send_msg(msg)
+        data = self.socket.recv(1024).decode("utf-8")  # 接收问好信息
+        print(data)
         while True:
-            if self.recv_msg():
-                msg=input("\n我：")
-                self.send_msg(msg)
-                if msg == "exit":
-                    print("聊天室已关闭")
-                    break
+            msg=input("\n我：")
+            if msg == "exit":
+                print("聊天室已关闭")
+                break
+            self.send_msg(msg)
+            self.recv_msg()
+
+
 
 if __name__ == '__main__':
-    cc = ChatClient(username="小明", port=9999)
+    cc = ChatClient(username="小明")
     cc.main()
--- ChatRoom/server.py
+++ ChatRoom/server.py
@@ -3,38 +3,48 @@ import time
 import threading
 import requests
 import json
+import struct
 
+ip_port = ("127.0.0.1", 9999)
 
 class ChatServer:
-    def __init__(self, port, key):
+    def __init__(self, key):
         # 绑定服务器的ip和端口，注意以tuple的形式
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind(("0.0.0.0", port))
+        self.socket.bind(ip_port)
         self.socket.listen()
         # 图灵机器人，授权码
         self.key = key  # 存上你自己的key
-        print("正在监听 127.0.0.1 ：{}...".format(port))
+        print("正在监听 127.0.0.1 ：{}...".format(ip_port[1]))
 
+    def recive(self, conn):
+        msg = conn.recv(4)
+        json_length = struct.unpack("i", msg)[0]  # 获取数据包长度，避免粘包
+        json_data = conn.recv(json_length)
+        return json.loads(json_data)
 
-    def tcplink(self, sock, addr):
+    def send_msg(self, conn, msg):
+        msg = msg.encode("utf-8")
+        msg_length = struct.pack("i", len(msg))
+        conn.send(msg_length)
+        conn.send(msg)
+
+    def tcplink(self, conn, addr):
         # 每次连接，开始聊天前，先欢迎下。
-        sock.send("你好，欢迎来到机器人聊天器！".encode("utf-8"))
+        conn.send("你好，欢迎来到机器人聊天器！".encode("utf-8"))
         while True:
-            data = sock.recv(8).decode("utf-8")
-            print(sock.getpeername())
-            print(sock.getsockname())
-            print(sock.fileno())
-            username = data.split("::")[0]
-            msg = data.split("::")[1]
-            if msg == "exit":
-                break
-            if msg:
-                print("【"+username+"】 "+time.strftime('%Y-%m-%d:%H:%M:%S',time.localtime(time.time())))
+            try:
+                json_data = self.recive(conn)
+                msg = json_data["msg"]
+                name = json_data["name"]
+                print("【"+name+"】 "+time.strftime('%Y-%m-%d:%H:%M:%S',time.localtime(time.time())))
                 print(msg)
                 response = self.get_response(msg)
-                sock.send(response.encode("utf-8"))
-        sock.close()
-        print("与 {} 结束聊天！".format(username))
+                self.send_msg(conn, response)
+            except:
+                conn.close()
+                print("与 {} 结束聊天！".format(name))
+                break
 
     def get_response(self, info):
         # 调用图灵机器人API
@@ -46,13 +56,13 @@ class ChatServer:
 
     def main(self):
         while True:
-            sock, addr = self.socket.accept()
-            t = threading.Thread(target=self.tcplink, args=(sock, addr))
+            conn, addr = self.socket.accept()
+            t = threading.Thread(target=self.tcplink, args=(conn, addr))
             t.start()
 
 
 if __name__ == '__main__':
     with open("key") as f:
         key = f.read()
-    cs = ChatServer(port=9999, key=key)
+    cs = ChatServer(key=key)
     cs.main()
